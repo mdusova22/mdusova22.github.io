de352b292762e5c7bca0fd6aa7a5ff1486594a01
mdusova22 <devtethys@gmail.com>
2024-07-31 11:34:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,57 @@ window.addEventListener('scroll', ()=> {
         arrowEl.style.opacity = "0";
     }
 });
+
+/*gece modu*/ (function(window, document, undefined){
+  "use strict";
+  var nightMode = document.cookie.indexOf("nightMode=true") !== -1;
+  var lightMode = document.cookie.indexOf("nightMode=false") !== -1;
+  if (nightMode){
+    document.documentElement.className += " night-mode";
+ }else{
+	 document.documentElement.className += " light-mode";
+ }
+  const userPrefersDark =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const userPrefersLight =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: light)").matches;
+  //Önceden lihtmode seçilmemiş ise
+  if (!lightMode){
+    if (userPrefersDark){
+      //Cihaz koyu mod kullanıyor ise
+      document.documentElement.className += " night-mode";
+   }
+ }else{
+	if (userPrefersLight){
+      //Cihaz koyu mod kullanıyor ise
+      document.documentElement.className += " light-mode";
+   }
+ }
+})(window, document);
+(function(window, document, undefined){
+  "use strict";
+  var nav = document.querySelector(".theme-mode");
+  //HTML .theme-mode class butonu ekle
+  nav.innerHTML +=
+    '<span id="night-mode"><a role="button" title="nightMode" href="javascript:void(0);">🌓</a></span>';
+  var nightMode = document.querySelector("#night-mode");
+  nightMode.addEventListener(
+    "click",
+    function(event){
+      event.preventDefault();
+      document.documentElement.classList.toggle("light-mode, night-mode");
+      if (document.documentElement.classList.contains("night-mode")){
+        //nightMode cookie etkinleştir
+        return (document.cookie =
+          "nightMode=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/; secure;");
+     }
+      //nightMode cookie etkisizleştir
+      document.cookie =
+        "nightMode=false; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/; secure;";
+   },
+    false
+  );
+})(window, document);
+/*gece modu*/
